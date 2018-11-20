45d48925abf4166ffb95770ee1ff8b614dd3fec1
Martin Nielsen <mnn@magenta-aps.dk>
2018-11-20 11:59:40 +0100

    Added bootstrap logic for datastore. Expanded model.

---
+++ backend/src/main/java/dk/opendesk/foundationapplication/patches/InitialStructure.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.opendesk.foundationapplication.patches;
+
+import org.alfresco.repo.admin.patch.AbstractPatch;
+
+/**
+ *
+ * @author martin
+ */
+public class InitialStructure extends AbstractPatch{
+
+    @Override
+    protected String applyInternal() throws Exception {
+        return null;
+    }
+    
+    
+    
+}
